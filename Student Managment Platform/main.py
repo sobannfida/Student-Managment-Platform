ba510206d7597cfecb45ba0b516b14758bb7038b
Soban Fida <sobann.fida@gmail.com>
2026-06-30 11:10:28 +0500

    Improve error handling in main function
    Refactor main loop to handle invalid input and exceptions. Not the best for now but good way to start the error handling

--- Student Managment Platform/main.py
+++ Student Managment Platform/main.py
@@ -2,53 +2,52 @@ students = []
 # Main Function
 
 def main():
-    while True:    
-        choice = int(input("1.Add Student\n2.Delete Student's Data\n3.Update Student's Data\n4.View Student's Data\n5.Search Student\n6.Exit\n:  "))
-        if choice == 1:
-            add_student_data()
-        elif  choice == 2:
-            delete_student_data()
-        elif choice == 3:
-            update_student_data()
-        elif choice == 4:
-            view_student_data()
-        elif choice == 5:
-            search_student()
-        elif  choice == 6:
-            exit_program()
-        else:
-            print("Enter Valid Input!")
-            again = input("Do you want to try again? y/n").lower()
-            if again == "y":
-                main()
+    while True:
+        try:
+            choice = int(input("1.Add Student\n2.Delete Student's Data\n3.Update Student's Data\n4.View Student's Data\n5.Search Student\n6.Exit\n:  "))
+            if choice == 1:
+                add_student_data()
+            elif  choice == 2:
+                delete_student_data()
+            elif choice == 3:
+                update_student_data()
+            elif choice == 4:
+                view_student_data()
+            elif choice == 5:
+                search_student()
+            elif  choice == 6:
+                exit_program()
             else:
-                quit()
+                print("Enter Valid Input!")
+        except ValueError:
+            print("Invalid input. Please enter a choice from 1 to 6")
 
 # Add Student's Data
 def add_student_data():
 
     # Keep asking for a roll number until a unique one is entered
     while True:
+        try:
+            # Ask user for a roll number
+            roll_no = int(input("Enter Student's Roll Number: "))
 
-        # Ask user for a roll number
-        roll_no = int(input("Enter Student's Roll Number: "))
+            # Assume the roll number is unique
+            duplicate = False
 
-        # Assume the roll number is unique
-        duplicate = False
+            # Check every existing student
+            for student in students:
 
-        # Check every existing student
-        for student in students:
+                # If another student already has this roll number
+                if student["roll_no"] == roll_no:
+                    duplicate = True
+                    print("Roll Number already exists!")
+                    break      # Stop checking further students
 
-            # If another student already has this roll number
-            if student["roll_no"] == roll_no:
-                duplicate = True
-                print("Roll Number already exists!")
-                break      # Stop checking further students
-
-        # If no duplicate was found, leave the while loop
-        if not duplicate:
-            break
-
+            # If no duplicate was found, leave the while loop
+            if not duplicate:
+                break
+        except ValueError:
+            print("Enter a valid number with integer data type!")        
     # Now that we have a unique roll number,
     # ask for the remaining student information
     name = input("Enter Student's Name: ")
@@ -99,9 +98,11 @@ def update_student_data():
 
 # View Student's Data
 def view_student_data():
-    for student in students:
-        print(f"Name: {student['name']}, Roll Number: {student['roll_no']}, Class: {student['class']}, Section: {student['section']}")
-
+    if students:
+        for student in students:
+            print(f"Name: {student['name']}, Roll Number: {student['roll_no']}, Class: {student['class']}, Section: {student['section']}")
+    else:
+        print("No Student found!")
 # Exit
 def exit_program():
     quit()
